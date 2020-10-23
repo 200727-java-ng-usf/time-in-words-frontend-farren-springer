08a422dd2ff66062d2e58907bcb192dbfb577a5d
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-10-23 15:05:07 -0500

    HttpClientModule import

--- time-in-words/src/app/app.module.ts
+++ time-in-words/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
