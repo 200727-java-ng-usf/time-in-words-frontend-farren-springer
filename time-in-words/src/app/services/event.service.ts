8dd62a62d98bc901c06086c684fd94696d509473
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-10-23 15:07:14 -0500

    event service

---
+++ time-in-words/src/app/services/event.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment as env } from '../../environments/environment';
+import { Event } from '../models/event';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  private event: BehaviorSubject<Event>;
+  event$: Observable<Event>;
+
+  constructor(private http: HttpClient) {
+    console.log('Instantiating EventService');
+  }
+
+  getAllEvents(){
+    console.log("Fetching from backend...")
+    console.log("URL: "+`${env.API_URL}/events`);
+    return this.http.get(`${env.API_URL}/events`,{responseType:'json',observe:"response"});
+  }
+}
